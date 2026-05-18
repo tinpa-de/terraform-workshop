6ffb8a1874223326e34e04cc669eb627b9cd92f3
Tino <50589759+tinpa-de@users.noreply.github.com>
2026-05-18 09:22:56 +0200

    Updated resources

---
+++ terraform/initialize-lock-db/variables.tf
@@ -0,0 +1,4 @@
+variable "dynamodb_table_name" {
+  description = "The name of the table, which should contain the terraform lock."
+  type        = string
+}
---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-nl-devk"
+    key    = "infrastructure-NAME" # Change NAME to your name to avoid conflicts with other workshop participants
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.8.0"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  alias  = "frankfurt"
+  region = var.region
+}
+
+provider "aws" {
+  alias  = "virginia"
+  region = "us-east-1"
+}
