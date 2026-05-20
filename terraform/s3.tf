d6feabec7bb24449623740ab78877d9b4df2e0f0
Juli Siems <jsiems@protonmail.com>
2026-05-20 14:42:47 +0200

    bonus task

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -26,7 +26,7 @@ resource "aws_s3_bucket_policy" "allow_public_website_access" {
         Version = "2012-10-17"
         Statement = [{
             Effect    = "Allow"
-            Principal = "*"
+            Principal = "cloudfront.amazonaws.com"
             Action    = "s3:GetObject"
             Resource  = "arn:aws:s3:::juli-walkthrough1-workshop-static-page/*"
         }]
@@ -40,6 +40,55 @@ resource "aws_s3_bucket_website_configuration" "bucket_website_configuration" {
     suffix = "index.html"
   }
 
+}
+
+resource "aws_cloudfront_origin_access_control" "default" {
+  name                              = "default-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.my_first_s3_bucket.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id                = "s3_origin"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3_origin"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
 
 output "website_url" {
