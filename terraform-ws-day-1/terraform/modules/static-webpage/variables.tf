11ddc1bef86aa46f757ee9b7702f896ecd0b1d81
Jasper Fülle <jasperfue@live.de>
2026-06-02 17:13:57 +0200

    2 complete

---
+++ terraform-ws-day-1/terraform/modules/static-webpage/variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  type        = string
+  description = "Eine kurze Kennung für diese Website. Wird zur Konstruktion eines eindeutigen Bucket-Namens verwendet."
+}
+
+variable "filepath" {
+  type = string
+  description = "Der lokale Pfad zur hochzuladenden HTML-Datei, relativ zum Verzeichnis terraform/."
+}
