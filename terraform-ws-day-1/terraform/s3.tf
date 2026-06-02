6a2c73ff4bf48325550cc05a0490df9e921190a2
Jasper Fülle <jasperfue@live.de>
2026-06-02 15:25:23 +0200

    1.2

--- terraform-ws-day-1/terraform/s3.tf
+++ terraform-ws-day-1/terraform/s3.tf
@@ -2,3 +2,11 @@ resource "aws_s3_bucket" "jasper-workshop-static-page" {
   bucket = "jasper-nl-20260602"
   provider = aws.frankfurt
 }
+
+resource "aws_s3_object" "jasper-workshop-static-page-index" {
+  bucket = aws_s3_bucket.jasper-workshop-static-page.id
+  key = "index.html"
+  source = "../resources/static-page/index.html"
+  content_type = "text/html"
+  etag = filemd5("../resources/static-page/index.html")
+}
