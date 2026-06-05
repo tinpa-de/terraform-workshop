be1003f0dc12a27066f39c5fc1018035973ded8f
Jasper Fülle <jasperfue@live.de>
2026-06-05 09:11:17 +0200

    add solution of day 1 task 2

---
+++ terraform-ws-day-1/solutions/task-2/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.8.0"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  alias  = "frankfurt"
+  region = var.region
+}
+
+module "static_page_1" {
+  source = "./modules/static-webpage"
+  providers = {
+    aws.frankfurt = aws.frankfurt
+  }
+  filepath = "../resources/static-page/index.html"
+  name     = "VORNAME-static-page-1"
+}
+
+output "website_url_1" {
+  value = module.static_page_1.website_url
+}
+
+module "static_page_2" {
+  source = "./modules/static-webpage"
+  providers = {
+    aws.frankfurt = aws.frankfurt
+  }
+  filepath = "../resources/static-page-2/index.html"
+  name     = "VORNAME-static-page-2"
+}
+
+output "website_url_2" {
+  value = module.static_page_2.website_url
+}
+
+module "static_page_3" {
+  source = "./modules/static-webpage"
+  providers = {
+    aws.frankfurt = aws.frankfurt
+  }
+  filepath = "../resources/static-page-3/index.html"
+  name     = "VORNAME-static-page-3"
+}
+
+output "website_url_3" {
+  value = module.static_page_3.website_url
+}
+
+
---
+++ terraform-ws-day-1/solutions/task-2/modules/static-webpage/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    aws = {
+      source                = "hashicorp/aws"
+      configuration_aliases = [aws.frankfurt]
+    }
+  }
+}
+
+resource "aws_s3_bucket" "jasper-workshop-static-page" {
+  bucket   = "workshop-${var.name}"
+  provider = aws.frankfurt
+}
+
+resource "aws_s3_object" "jasper-workshop-static-page-index" {
+  bucket       = aws_s3_bucket.jasper-workshop-static-page.id
+  provider     = aws.frankfurt
+  key          = "index.html"
+  source       = var.filepath
+  content_type = "text/html"
+  etag         = filemd5(var.filepath)
+}
+
+resource "aws_s3_bucket_public_access_block" "jasper-workshop-static-page-public-access-block" {
+  bucket   = aws_s3_bucket.jasper-workshop-static-page.id
+  provider = aws.frankfurt
+}
+
+resource "aws_s3_bucket_policy" "jasper-workshop-static-page-policy" {
+  bucket     = aws_s3_bucket.jasper-workshop-static-page.id
+  provider   = aws.frankfurt
+  policy     = data.aws_iam_policy_document.allow_access_to_bucket.json
+  depends_on = [aws_s3_bucket_public_access_block.jasper-workshop-static-page-public-access-block]
+}
+
+data "aws_iam_policy_document" "allow_access_to_bucket" {
+  statement {
+    # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document#principals-and-not_principals
+    principals {
+      identifiers = ["*"]
+      type        = "*"
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.jasper-workshop-static-page.arn}/*"
+    ]
+  }
+}
+
+
+resource "aws_s3_bucket_website_configuration" "jasper-workshop-static-page-website-configuration" {
+  bucket   = aws_s3_bucket.jasper-workshop-static-page.id
+  provider = aws.frankfurt
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+output "website_url" {
+  value = "http://${aws_s3_bucket_website_configuration.jasper-workshop-static-page-website-configuration.website_endpoint}"
+}
